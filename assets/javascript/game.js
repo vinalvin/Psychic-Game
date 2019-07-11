ee5577cadc802dfba4e0dc0f1fec01288f0c0f91
Junming Zhang <vinalvin1010@gmail.com>
2019-07-11 15:56:42 -0700

    Redo all over again

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,38 @@
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var lettersGuessed = []
-var computerGuess = []
+var lettersGuessed = [];
+var computerGuess = [];
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-function setHtml() {
-    var htmlTemplate = "<p>Guess what letter I'm thinking of.</p>" +
-        "<p>Wins: " + wins + "</p>" +
-        "<p>Losses: " + losses + "</p>" +
-        "<p>Guesses left: " + guessesLeft + "</p>" +
-        "<p>Your guesses so far: " + lettersGuessed + "</p>";
-    document.querySelector("#game").innerHTML = htmlTemplate;
+function pickLetter() {
+    var random = Math.floor(Math.random() * computerChoices.length);
+    computerGuess = computerChoices[random];
+    console.log("computerGuess", computerGuess);
 }
+pickLetter()
 
-window.onload = function() {
-    var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    computerGuess.push(compGuess);
-    console.log(computerGuess[0]);
-    setHtml()
-
-    document.onkeyup = function(event) {
-        var playerGuess = event.key;
-        lettersGuessed.push(playerGuess);
-        console.log('computerGuess[0]', computerGuess[0]);
-        console.log('playerGuess', playerGuess)
-        if ((playerGuess === computerGuess[0]) && (guessesLeft > 0)) {
-            wins++;
-            guessesLeft = 9;
-            lettersGuessed.length = 0;
-            computerGuess.length = 0;
-            var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-            computerGuess.push(compGuess);
-            console.log(computerGuess[0]);
-        } else if ((playerGuess !== computerGuess[0]) && (guessesLeft > 0)) {
-            guessesLeft = guessesLeft - 1;
-        } else {
-            losses++;
-            guessesLeft = 9;
-            lettersGuessed.length = 0;
-            computerGuess.length = 0;
-            var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-            computerGuess.push(compGuess);
-            console.log(computerGuess[0]);
-        }
-        setHtml()
+document.onkeypress = function(event) {
+    if (event.key === computerGuess) {
+        wins++;
+        document.querySelector("#wins").innerHTML = wins;
+        pickLetter()
+    } else {
+        guessesLeft--;
+        document.querySelector("#guessesLeft").innerHTML = guessesLeft;
+        computerGuess.push(event.key);
+        document.querySelector("#computerGuess").innerHTML = computerGuess.join();
     }
+    console.log("wins", wins);
+    console.log("guessesLeft", guessesLeft);
+
+    if (guessesLeft == 0) {
+        losses++;
+        document.querySelector("#losses").innerHTML = losses;
+        guesses = 9;
+        computerGuess = [];
+        document.querySelector("#computerGuess").innerHTML = computerGuess.join();
+        pickLetter();
+    }
+    console.log("losses", losses);
 }
